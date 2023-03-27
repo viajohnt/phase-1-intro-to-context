ee2737a03975d4787ac65fa33c99f2180540da94
John Truong <viajohnt@gmail.com>
2023-03-27 16:21:07 -0600

    A commit

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+const createEmployeeRecord = function(element) {
+    return {
+        firstName: element[0],
+        familyName: element[1],
+        title: element[2],
+        payPerHour: element[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+    
+const createEmployeeRecords = function(elementData){
+    return elementData.map(element => createEmployeeRecord(element));
+    }
+
+    let createTimeInEvent = function(employee, dateStamp){
+        let [date, hour] = dateStamp.split(' ')
+    
+        employee.timeInEvents.push({
+            type: "TimeIn",
+            hour: parseInt(hour, 10),
+            date,
+        })
+    
+        return employee
+    }
